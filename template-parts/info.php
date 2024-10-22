0cff6218358e9e0269d86c884fd4093b0863324c
Wagner <wagnerbeethoven@gmail.com>
2024-10-22 18:41:13 -0300

    slogan!

---
+++ template-parts/info.php
@@ -0,0 +1,5 @@
+<div class="col-12 col-lg-8 col-xlg-6 lead">
+  <p>
+    Designer há mais de dez anos construindo e projetando soluções úteis para as pessoas, com vasta experiência em usabilidade, entusiasta da acessibilidade e da tecnologia assistiva.
+  </p>
+</div>
